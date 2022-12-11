f436313a19a20224111d27565ed594622e83361b
Daianywhere <qwerty190854@goo.jp>
2022-12-11 18:57:38 +0900

    検査用関数を作成

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const cq = (query) => {
+  const elemno = document.querySelectorAll(query);
+  console.log('count: ' + elemno);
+}
+
 const clickBySelector = (selector) => {
   const elem = document.querySelector(selector);
   if (elem) {
