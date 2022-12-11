7c5854d6f75dd3cc3f855b1279ab17899f910d97
Daianywhere <qwerty190854@goo.jp>
2022-12-11 13:26:43 +0900

    クリック関数を追加

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const clickBySelector = (selector) => {
+  const elem = document.querySelector(selector);
+  if (elem) {
+    elem.click();
+  } else {
+    console.log('no element: ' + selector);
+  }
+}
+
 const data = [
   {
     url: "https://*/*",
