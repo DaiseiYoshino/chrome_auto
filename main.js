ca892f0cdd0c40b37bc7a781464e32b0c9fff56d
Daianywhere <qwerty190854@goo.jp>
2022-12-11 13:13:32 +0900

    設定を外部に出した

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-document.addEventListener('keypress', (e) => {
-  const key = e.key;
-  if (key == 'x') console.log('key x pressed');
-});
+const data = [
+  {
+    url: "https://*/*",
+    commands: {
+      'x': () => console.log('key x pressed')
+    }
+  }
+]
+
+for (const d of data) {
+  if (document.location.href.match(d.url)) {
+    const commands = d.commands
+    document.addEventListener('keypress', (e) => {
+      const key = e.key;
+      if (key in commands) commands[key]();
+    });
+  }
+}
