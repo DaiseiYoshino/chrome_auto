4bac55059ee918a8cdf592747a97ec52800b5d0b
Daianywhere <qwerty190854@goo.jp>
2022-12-14 00:47:33 +0900

    main.jsにコメント追加等

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
+// 設定されている各データについてループを回す
 for (const d of data) {
+  // データの対象URLに引っかかった1つ目について対応する処理を行う
   if (document.location.href.match(d.url)) {
-    const commands = d.commands
-    document.addEventListener('keypress', (e) => {
-      const key = e.key;
-      if (key in commands) {
-        e.preventDefault();
-        commands[key]()
-      };
-    });
 
+    // キー入力コマンドの設定
+    const commands = d.commands
+    if (commands) {
+      document.addEventListener('keypress', (e) => {
+        const key = e.key;
+        if (key in commands) {
+          e.preventDefault();
+          commands[key]()
+        };
+      });
+    }
+
+    // 自動実行関数があれば実行する
     if (d.auto) {
       d.auto();
     }
+
+    return;
   }
 }
