b2bb3cf6a060040069a00e73ee78a74a3b4237e2
Daisei Yoshino <daisei.yoshino@sencorp.co.jp>
2023-03-27 10:33:06 +0900

    tmp

--- js/screenshot.js
+++ js/screenshot.js
@@ -3,5 +3,27 @@
 */
 
 const getScreenShot = () => {
+  let tabData = {};
+  let shared = {
+    imageDirtyCutAt: 0,
+    imageDataURL: 0,
+    originalScrollTop: 0
+  };
+  // 現在のタブ情報を取得
+  chrome.windows.getCurrent((win) => {
+    chrome.tabs.query(
+      {active: true, windowID: win.id},
+      (tabs) => {
+        tabData = tabs[0];
+      }
+    );
+  });
 
+  // スクショ開始
+  chrome.tabs.sendMessage(
+    tabData.id,
+    {
+      action: 'screenshotBegin'
+    }
+  );
 }
