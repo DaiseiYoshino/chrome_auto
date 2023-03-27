549644fa62e335788889aea4a232bfab6afc9d1a
Daisei Yoshino <daisei.yoshino@sencorp.co.jp>
2023-03-27 10:35:07 +0900

    タブ情報をsharedに入れ込む

--- js/screenshot.js
+++ js/screenshot.js
@@ -3,18 +3,23 @@
 */
 
 const getScreenShot = () => {
-  let tabData = {};
   let shared = {
     imageDirtyCutAt: 0,
     imageDataURL: 0,
-    originalScrollTop: 0
+    originalScrollTop: 0,
+    tab: {
+      id: 0,
+      url: '',
+      title: '',
+      hasVscrollbar: false
+    }
   };
   // 現在のタブ情報を取得
   chrome.windows.getCurrent((win) => {
     chrome.tabs.query(
       {active: true, windowID: win.id},
       (tabs) => {
-        tabData = tabs[0];
+        shared.tab = tabs[0];
       }
     );
   });
